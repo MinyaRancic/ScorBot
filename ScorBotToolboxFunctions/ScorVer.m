4e0a0c0107524aa222d6e160478a585744a2c55f
kutzer <kutzer@usna.edu>
2015-12-23 12:20:25 -0500

    Version update

--- ScorBotToolboxFunctions/ScorVer.m
+++ ScorBotToolboxFunctions/ScorVer.m
@@ -39,11 +39,13 @@ function varargout = ScorVer
 %   23Oct2015 - Updates to ScorSim* to include XYZPR and BSEPR teach modes. 
 %   23Oct2015 - Update to ScorXYZPR2BSEPR to allow user to select between
 %               elbow-up and elbow-down solutions.
+%   23Dec2015 - Updates to XYZPR, BSEPR, and Pose input functions
+%               (excluding ScorSim*) to clarify errors.
 
 A.Name = 'ScorBot Toolbox';
-A.Version = '2.2.5';
+A.Version = '2.2.6';
 A.Release = '(R2014a)';
-A.Date = '23-Oct-2015';
+A.Date = '23-Dec-2015';
 A.URLVer = 1;
 
 msg{1} = sprintf('MATLAB %s Version: %s %s',A.Name, A.Version, A.Release);
