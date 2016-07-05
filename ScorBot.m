0400b3d850a646bbb754dda3e6c4ef6669d23b0b
dsaiontz <david.saiontz@gmail.com>
2016-07-05 10:59:26 -0400

    Adding Set Methods

--- ScorBot.m
+++ ScorBot.m
@@ -37,5 +37,25 @@ classdef ScorBot < matlab.mixin.SetGet
         function Speed = get.Speed(obj)
             Speed = obj.ScorGetSpeed;
         end
+        
+        function obj = set.BSEPR(obj, value)
+            obj.BSEPR = value;
+        end
+        
+        function obj = set.XYZPR(obj, value)
+            obj.XYZPR = value;
+        end
+        
+        function obj = set.Pose(obj, value)
+            obj.Pose = value;
+        end
+        
+        function obj = set.Gripper(obj, value)
+            obj.Gripper = value;
+        end
+        
+        function obj = set.Speed(obj, value)
+            obj.Speed = value;
+        end
     end
 end
