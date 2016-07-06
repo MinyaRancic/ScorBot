538b8e1fa40c1ba216593780040b3f4e9426847c
dsaiontz <david.saiontz@gmail.com>
2016-07-06 10:28:52 -0400

    ScorSimBot Working

--- ScorSimBot.m
+++ ScorSimBot.m
@@ -1,20 +1,21 @@
-%classdef ScorSimBot < matlab.mixin.SetGet
-classdef ScorSimBot < hgsetget
+classdef ScorSimBot < matlab.mixin.SetGet
+%classdef ScorSimBot < hgsetget
     properties(GetAccess = 'public', SetAccess = 'public')
-        MoveTime
-        MoveType
+        %MoveTime
+        %MoveType
         BSEPR       %Joint angles
         XYZPR       %Task Space
         Pose        %Task Space (SE3)
         Gripper
         Speed
+        Simulation
     end
     
     methods(Access = 'public')
         function obj = ScorSimBot
-            ScorSimInit;
-            ScorSimHome;
-            Siminitialize(obj);
+            obj.Simulation = ScorSimInit;
+            ScorSimPatch(obj.Simulation);
+            obj.Siminitialize;
         end
         
         function delete(obj)
@@ -23,78 +24,76 @@ classdef ScorSimBot < hgsetget
         end
         
         function Siminitialize(obj)
-            obj.MoveTime = ScorSimGetMoveTime;
-            obj.MoveType = 'LinearJoint';
-            obj.BSEPR = ScorSimGetBSEPR;
-            obj.XYZPR = ScorSimGetXYZPR;
-            obj.Pose = ScorSimGetPose;
-            obj.Gripper = ScorSimSimGetGripper;
-            obj.Speed = ScorGetSpeed;
+            %obj.Simulation = ScorSimInit;
+            %obj.MoveType = 'LinearJoint';
+            obj.BSEPR = ScorSimGetBSEPR(obj.Simulation);
+            obj.XYZPR = ScorSimGetXYZPR(obj.Simulation);
+            obj.Pose = ScorSimGetPose(obj.Simulation);
+            obj.Gripper = ScorSimGetGripper(obj.Simulation);
         end
     end
     
     methods
         function BSEPR = get.BSEPR(obj)
-            disp('TEST')
-            BSEPR = ScorSimGetBSEPR;
+            BSEPR = ScorSimGetBSEPR(obj.Simulation);
         end
         
         function XYZPR = get.XYZPR(obj)
-            XYZPR = ScorSimGetXYZPR;
+            XYZPR = ScorSimGetXYZPR(obj.Simulation);
         end
         
         function Pose = get.Pose(obj)
-            Pose = ScorSimGetPose;
+            Pose = ScorSimGetPose(obj.Simulation);
         end
         
         function Gripper = get.Gripper(obj)
-            Gripper = ScorSimGetGripper;
+            Gripper = ScorSimGetGripper(obj.Simulation);
         end
         
-        function Speed = get.Speed(obj)
-            Speed = ScorSimGetSpeed;
-        end
+%         function Speed = get.Speed(obj)
+%             Speed = ScorSimGetSpeed(obj.Simulation);
+%         end
         
         function set.BSEPR(obj, value)
             obj.BSEPR = value;
-            ScorSimSetBSEPR(value, 'MoveType', obj.MoveType);
+            ScorSimSetBSEPR(obj.Simulation, value);
         end
         
         function set.XYZPR(obj, value)
             obj.XYZPR = value;
-            ScorSimSetXYZPR(value, 'MoveType', obj.MoveType);
+            ScorSimSetXYZPR(obj.Simulation, value);
         end
         
         function set.Pose(obj, value)
             obj.Pose = value;
-            ScorSimSetPose(value);
+            ScorSimSetPose(obj.Simulation, value);
         end
         
         function set.Gripper(obj, value)
             obj.Gripper = value;
-            ScorSimSetGripper(value);
+            ScorSimSetGripper(obj.Simulation, value);
         end
-        
-        function set.Speed(obj, value)
-            obj.Speed = value;
-            ScorSimSetSpeed(value);
-        end
-        
-        function set.MoveTime(obj, value)
-            obj.MoveTime = value;
-            ScorSimSetMoveTime(value);
-        end
-        
-        function set.MoveType(obj, value)
-            switch(value)
-                case 'LinearJoint'
-                    obj.MoveType = 'LinearJoint';
-                case 'LinearTask'
-                    obj.MoveType = 'LinearTask';
-                otherwise
-                    error('Move Type must be LinearTask or LinearJoint');   
-            end
-        end
+%         
+%         function set.Speed(obj, value)
+%             obj.Speed = value;
+%             ScorSimSetSpeed(obj.Simulation, value);
+%         end
+%         
+%         function set.MoveTime(obj, value)
+%             obj.MoveTime = value;
+%             ScorSimSetMoveTime(value);
+%         end
+%         
+%         function set.MoveType(obj, value)
+%             switch(value)
+%                 case 'LinearJoint'
+%                     obj.MoveType = 'LinearJoint';
+%                 case 'LinearTask'
+%                     obj.MoveType = 'LinearTask';
+%                 otherwise
+%                     error('Move Type must be LinearTask or LinearJoint');   
+%             end
+%         end
         
     end
 end
