4e8949518548bb1527832a5cb8ab5a12d4bdf508
whatsGravity <rancic.minya@gmail.com>
2016-07-07 10:51:26 -0400

    pls stop

---
+++ ScorBotToolboxFunctions/ScorSendXYZPR.m
@@ -0,0 +1,36 @@
+function [ output_args ] = ScorSendXYZPR( input_args )
+% SCORSENDXYZPR sends the current XYZPR or a specified XYZPR value
+%   ScorSendXYZPR(udpS) sends the current XYZPR value from ScorBot to the
+%   UDP sender specified in udpS.
+%
+%   ScorSendXYZPR(udpS,XYZPR) sends the specified XYZPR value to the UDP
+%   sender specified in udpS.
+%
+% See also ScorInitSender ScorInitReceiver ScorReceiveXYZPR
+%
+%% Check UDP Receiver
+switch class(udpR)
+    case 'dsp.UDPReceiver'
+        % Valid UDPReceiver
+    otherwise
+        error('ScorReceive:BadReceiver',...
+            ['Specified UDP Receiver must be valid.',...
+             '\n\t-> Use "udpR = ScorRcver(port);" with a valid port ',...
+             '\n\t   number to create a valid UDP Receiver.']);
+end
+
+%% Receive message
+% Receive message
+dataReceived = step(udpR);
+% Convert message
+msg_Rsvd = char(dataReceived');
+% Parse message
+XYZPR = str2num(msg_Rsvd);
+
+%% Check message
+if ~isnumeric(XYZPR) || numel(XYZPR) ~= 5
+    XYZPR = []; % return an empty set
+end
+
+end
+
