cb409560c0d6c6b85d1871ab7401049c704d177f
dsaiontz <david.saiontz@gmail.com>
2016-07-08 11:15:37 -0400

    Added Set and Get Functions to ScorBot6Axis

--- ScorBot6Axis.m
+++ ScorBot6Axis.m
@@ -8,6 +8,7 @@ classdef ScorBot6Axis < matlab.mixin.SetGet
         Pose
         Simulation
         Gripper
+        PrevSpot
     end
     
     methods (Access = 'public')
@@ -22,5 +23,48 @@ classdef ScorBot6Axis < matlab.mixin.SetGet
         end
     end
     
+    methods
+        function BSEPR = getBSEPR(obj) %Returns BSEPR
+            BSEPR = ScorSimGetBSEPR(obj.Simulation);
+        end
+        
+        function XYZPR = getXYZPR(obj) %Returns XYZPR
+            XYZPR = ScorSimGetXYZPR(obj.Simulation);
+        end
+        
+        function Pose = getPose(obj) %Returns Pose
+            Pose = ScorSimGetPose(obj.Simulation);
+        end
+        
+        function Gripper = getGripper(obj) %Returns Gripper
+            Gripper = ScorSimGetGripper(obj.Simulation);
+        end
+        
+        function setBSEPR(obj, value) %Sets BSEPR to value and sets
+            %             PrevSpot to most recent BSEPR
+            obj.PrevSpot = obj.BSEPR;
+            obj.BSEPR = value;
+            ScorSimSetBSEPR(obj.Simulation, value);
+            obj.XYZPR = ScorBSEPR2XYZPR(obj.BSEPR);
+            obj.Pose = ScorBSEPR2Pose(obj.BSEPR);
+        end
+        
+        function setXYZPR(obj, value) %Sets XYZPR to value and sets
+            %             PrevSpot to most recent BSEPR
+            obj.PrevSpot = ScorXYZPR2BSEPR(obj.XYZPR);
+            obj.XYZPR = value;
+            ScorSimSetXYZPR(obj.Simulation, value);
+            obj.BSEPR = ScorXYZPR2BSEPR(obj.XYZPR);
+            obj.Pose = ScorXYZPR2Pose(obj.XYZPR);
+        end
+        
+        function setPose(obj, value) %Sets Pose to value and sets
+            %             PrevSpot to most recent BSEPR
+            obj.PrevSpot = ScorPose2BSEPR(obj.Pose);
+            obj.Pose = value;
+            ScorSimSetPose(obj.Simulation, value);
+            obj.BSEPR = ScorPose2BSEPR(obj.Pose);
+            obj.XYZPR = ScorPose2XYZPR(obj.Pose);
+        end
+    end
 end
-
